09099691c8085a66c5c27432302955637ba5b193
Abirami R <124359550+Abimacaw@users.noreply.github.com>
2024-10-09 14:20:57 +0530

    Sensors data included

---
+++ final_ROV.ino
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+const int adcPin = A1;
+  
+// calculate your own m using ph_calibrate.ino
+// When using the buffer solution of pH4 for calibration, m can be derived as:
+ // m = (pH7 - pH4) / (Vph7 - Vph4)
+const float m = -1.06;
+const float  OffSet = 1.172;
+float V, P;
+
+#define TdsSensorPin A2
+#define VREF 5.0      // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30           // sum of sample point
+int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+float averageVoltage = 0,tdsValue = 0,temperature = 25;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(TdsSensorPin,INPUT);
+}
+ void loop()
+ {
+  //Connect sensor to Analog 0
+  V = analogRead(0) * 5.00 / 1024;     //Sensor output voltage
+  P = (V - OffSet) * 250;             //Calculate water pressure
+
+  Serial.print("Voltage:");
+ Serial.print(V, 3);
+ Serial.println("V");
+
+  Serial.print(" Pressure:");
+  Serial.print(P, 1);
+  Serial.println(" KPa");
+  Serial.println();
+  static unsigned long analogSampleTimepoint = millis();
+   if(millis()-analogSampleTimepoint > 40U)     //every 40 milliseconds,read the analog value from the ADC
+   {
+     analogSampleTimepoint = millis();
+     analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+     analogBufferIndex++;
+     if(analogBufferIndex == SCOUNT) 
+         analogBufferIndex = 0;
+   }   
+   static unsigned long printTimepoint = millis();
+   if(millis()-printTimepoint > 800U)
+   {
+      printTimepoint = millis();
+      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
+      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+      //Serial.print("voltage:");
+      //Serial.print(averageVoltage,2);
+      //Serial.print("V   ");
+      Serial.print("TDS Value:");
+      Serial.print(tdsValue,0);
+      Serial.println("ppm");
+   }
+}
+int getMedianNum(int bArray[], int iFilterLen) 
+{
+      int bTab[iFilterLen];
+      for (byte i = 0; i<iFilterLen; i++)
+      bTab[i] = bArray[i];
+      int i, j, bTemp;
+      for (j = 0; j < iFilterLen - 1; j++) 
+      {
+      for (i = 0; i < iFilterLen - j - 1; i++) 
+          {
+        if (bTab[i] > bTab[i + 1]) 
+            {
+        bTemp = bTab[i];
+            bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+         }
+      }
+      }
+      if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+      else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+      return bTemp;
+   float Po = analogRead(adcPin) * 5.0 / 1024;
+   float phValue = 7 - (2.5 - Po) * m;
+   Serial.print("p h value = "); 
+   Serial.println(phValue);
+delay(2000);
+
+}
